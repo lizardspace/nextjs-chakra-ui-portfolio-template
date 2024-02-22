d388fef9623d7b456efde64b37570e897f0074fa
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-02-22 20:35:15 +0000

    gjhvbj,ghjhhjh

--- src/components/UI/logo.jsx
+++ src/components/UI/logo.jsx
@@ -4,7 +4,7 @@ import NextLink from "next/link";
 const Logo = () => {
   return (
     <NextLink href="/" passHref>
-      <chakra.a display="block" minW="200px" height="auto">
+      <chakra.a display="block" w={{ base: "150px", md: "200px" }} height="auto">
         <Image src="https://i.postimg.cc/4xH5nXTn/logofideo.png" alt="Logo" />
       </chakra.a>
     </NextLink>
